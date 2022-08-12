8cbaf58c30a39013e5115a66a1f82ef753bd0e52
LHD0617 <320388825@qq.com>
2022-08-12 17:14:31 +0800

    202208121713添加适应屏幕和保存图像功能

--- 01_GUI/Image.py
+++ 01_GUI/Image.py
@@ -16,7 +16,7 @@ from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QGroupBox
 from MessageClass import MessageClass
 
-ImageTypeStrList = ('Binarization', 'Grayscale', 'RGB565', 'RGB888')
+ImageTypeStrList = ('Binarization', 'Grayscale', 'RGB565')
 
 
 class MyImage(QGroupBox):
